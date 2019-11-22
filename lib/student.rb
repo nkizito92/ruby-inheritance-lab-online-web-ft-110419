71702bca68ad50819a26d180cca5f27458d3359c
Kizito Njoku <nkizito57@gmail.com>
2019-11-22 21:35:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,8 @@ class Student < User
   @@knowledge = []
   
     def initialize
-      @@knowledge << self 
+      @knowledge  = []
     end 
     
-    def self.all 
-      @@knowledge
-    end 
+    d
 end
